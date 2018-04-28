66d743170c8e90436b95ff7d743208836d86aa77
Klaus Fuhrmeiter <klaus.fuhrmeister@gmx.de>
2018-04-28 12:32:08 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script type="text/javascript" src="assets/scripts/main.js"></script>
+    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
